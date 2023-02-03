f4e5034760f891e8847bf647930fea15926fce6e
DongRyul Lee <al7ech@gmail.com>
2023-02-03 17:02:06 +0900

    Day5. Think about index

--- DR/util.py
+++ DR/util.py
@@ -4,6 +4,6 @@ def read_input():
     with open(f'{sys.argv[0].split(".")[0]}.input') as f:
         lines = f.readlines()
         for i in range(len(lines)):
-            lines[i] = lines[i].strip()
+            lines[i] = lines[i].replace("\n", "")
 
     return lines
