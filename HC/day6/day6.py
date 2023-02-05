3855fa19596dfb65156a73711e9b0350000b645f
heechan <102002122+supergermy@users.noreply.github.com>
2023-02-05 14:02:34 +0900

    Update day6.py

--- HC/day6/day6.py
+++ HC/day6/day6.py
@@ -4,16 +4,9 @@ signal = open(sys.argv[1]).readline().replace('\n','')
 
 length = int(sys.argv[2]) # 4? 14?
 
-def is_duplicate(a):
-
-    if len(a) == len(set(a)):
-        return 1
-    else:
-        return 0
-
 for i in range(0,len(signal)-(length-1)):
     marker = signal[i:i+length]
 
-    if is_duplicate(marker):
+    if len(marker) == len(set(marker)):
         print(i+length)
         break
