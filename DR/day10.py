d8792b6eee5adda14f38a2f130626f8852a6e84e
DongRyul Lee <al7ech@gmail.com>
2023-02-13 09:14:08 +0900

    Day10.
    Add signal_cycle func for clean code.

--- DR/day10.py
+++ DR/day10.py
@@ -1,29 +1,30 @@
 from util import read_input
 lines = read_input()
 
-signal_turns = list(range(20,230, 40))
-cur_turn = 1
+signal_cycle = list(range(20,230, 40))
+cur_cycle = 1
 X = 1
 part1 = 0
 
+def run_cycle():
+    render_CRT()
+    cur_cycle += 1
+
 def render_CRT():
-    print('#' if X-1 <= (cur_turn-1)%40 <= X+1 else '.', end='')
-    if cur_turn % 40 == 0:
+    print('#' if X-1 <= (cur_cycle-1)%40 <= X+1 else '.', end='')
+    if cur_cycle % 40 == 0:
         print('')
 
 for l in lines:
-    if signal_turns and signal_turns[0] - 1 <= cur_turn <= signal_turns[0]:
-        part1 += signal_turns[0] * X
-        signal_turns.pop(0)
+    if signal_cycle and signal_cycle[0] - 1 <= cur_cycle <= signal_cycle[0]:
+        part1 += signal_cycle[0] * X
+        signal_cycle.pop(0)
     if l.startswith("noop"):
-        render_CRT()
-        cur_turn += 1
+        run_cycle()
         continue
 
-    render_CRT()
-    cur_turn += 1
-    render_CRT()
-    cur_turn += 1
+    run_cycle()
+    run_cycle()
     X += int(l.split()[-1])
 
 print(part1)
