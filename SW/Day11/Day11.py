85a8226dfeb1474aca2aae9d8c7581eb5849e0fa
Bryan Roh <124754876+bryan9911@users.noreply.github.com>
2023-02-13 19:34:11 +0900

    Day11

--- SW/Day11/Day11.py
+++ SW/Day11/Day11.py
@@ -1,14 +1,17 @@
 import numpy as np
 
-count = np.zeros(8, dtype=int)
-MI = [[84, 66, 62, 69, 88, 91, 91],
-      [98, 50, 76, 99],
-      [72, 56, 94],
-      [55, 88, 90, 77, 60, 67],
-      [69, 72, 63, 60, 72, 52, 63, 78],
-      [89, 73],
-      [78, 68, 98, 88, 66],
-      [70]]
+count1 = np.zeros(8, dtype=int)
+count2 = np.zeros(8, dtype=int)
+MI1 = [[84, 66, 62, 69, 88, 91, 91],
+       [98, 50, 76, 99],
+       [72, 56, 94],
+       [55, 88, 90, 77, 60, 67],
+       [69, 72, 63, 60, 72, 52, 63, 78],
+       [89, 73],
+       [78, 68, 98, 88, 66],
+       [70]]
+
+MI2 = MI1[:]
 MO = [[2, 4, 7],
       [7, 3, 6],
       [13, 4, 0],
@@ -22,26 +25,26 @@ for m in MO:
     factor *= m[0]
 
 
-def monkey(num: int):
-    old = MI[num].pop(0)
+def monkey1(num: int):
+    old = MI1[num].pop(0)
     new = cal(old, num) // 3
     if (new % MO[num][0]) == 0:
-        MI[MO[num][1]].insert(-1, new)
+        MI1[MO[num][1]].insert(-1, new)
     else:
-        MI[MO[num][2]].insert(-1, new)
-    count[num] += 1
+        MI1[MO[num][2]].insert(-1, new)
+    count1[num] += 1
 
 
 def monkey2(num: int):
-    old = MI[num].pop(0)
+    old = MI2[num].pop(0)
     new = cal(old, num)
     if new > factor:
         new = new % factor
     if (new % MO[num][0]) == 0:
-        MI[MO[num][1]].insert(-1, new)
+        MI2[MO[num][1]].insert(-1, new)
     else:
-        MI[MO[num][2]].insert(-1, new)
-    count[num] += 1
+        MI2[MO[num][2]].insert(-1, new)
+    count2[num] += 1
 
 
 def cal(numin, n):
@@ -63,9 +66,17 @@ def cal(numin, n):
         return numin + 7
 
 
+for _ in range(0, 20):
+    for i in range(0, len(MI1)):
+        for j in range(0, len(MI1[i])):
+            monkey1(i)
+
 for _ in range(0, 10000):
-    for i in range(0, len(MI)):
-        for j in range(0, len(MI[i])):
+    for i in range(0, len(MI2)):
+        for j in range(0, len(MI2[i])):
             monkey2(i)
-res = sorted(count)
-print(res[-1]*res[-2])
+
+res1 = sorted(count1)
+res2 = sorted(count2)
+print(res1[-1]*res1[-2])
+print(res2[-1]*res2[-2])
