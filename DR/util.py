6a7bb759d7598bc162f0cd50fd6e63f971d68532
Bryan Roh <124754876+bryan9911@users.noreply.github.com>
2023-02-27 22:38:49 +0900

    AoC 2022

--- DR/util.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def read_input():
-    with open(f'{sys.argv[0].split(".")[0]}.input') as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            lines[i] = lines[i].replace("\n", "")
-
-    return lines
